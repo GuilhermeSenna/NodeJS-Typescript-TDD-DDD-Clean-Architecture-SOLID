82ace51c7250b8f84673c9cbf4c2f965c97a6150
zBiggui <bigguisenna@gmail.com>
2022-06-17 13:51:50 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from './signup'
+
+describe('SignUp', () => {
+    test('Should return 400 if no name is provided', () => {
+        // sut - System Under Test
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                // name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
+});
