b71c873ca15e13dfce0722498c63406377d39b5b
zBiggui <bigguisenna@gmail.com>
2022-06-17 19:57:08 -0300

    refactor: move duplicated to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,22 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helper/http-helper'
 
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
 
-        if (!httpRequest.body.name) {
-            return {
-                statusCode: 400,
-                body: new MissingParamError('name')
-            }
-        }
+        if (!httpRequest.body.name)
+            return badRequest(new MissingParamError('name'))
 
-        if (!httpRequest.body.email) {
-            return {
-                statusCode: 400,
-                body: new MissingParamError('email')
-            }
-        }
+        if (!httpRequest.body.email)
+            return badRequest(new MissingParamError('email'))
+
     }
 }
